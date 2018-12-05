fd53b3620e20d8c5ded5e45419106ea147aae8c5
cantalou <378112274@qq.com>
2018-12-05 17:23:20 +0800

    Update SampleApplication.java

--- app/src/main/java/com/cantalou/android/nativeutil/app/SampleApplication.java
+++ app/src/main/java/com/cantalou/android/nativeutil/app/SampleApplication.java
@@ -12,7 +12,7 @@ import android.app.Application;
  * @author LinZhiWei
  * @date 2017年02月14日 16:05
  * <p>
- * Copyright (c) 2017年, 4399 Network CO.ltd. All Rights Reserved.
+ * Copyright (c) 2017年,  Network CO.ltd. All Rights Reserved.
  */
 public class SampleApplication extends Application {
 
